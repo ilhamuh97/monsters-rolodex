dade1a2ddbbce87c84c5b153802db01473443d96
ilham muhammad <ilhamuh97@gmail.com>
2020-01-15 16:42:31 +0100

    Updates

---
+++ precache-manifest.514c769665cd4de90cb3d241849e3de9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd96618afb31bab7c441e0151cd6bad",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "15c39cb72f9d9b620313",
+    "url": "/monsters-rolodex/static/css/main.689b5aa3.chunk.css"
+  },
+  {
+    "revision": "29127bee117655bfd34b",
+    "url": "/monsters-rolodex/static/js/2.f33bfdc7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.f33bfdc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15c39cb72f9d9b620313",
+    "url": "/monsters-rolodex/static/js/main.ac614e41.chunk.js"
+  },
+  {
+    "revision": "66b7c417af356ee7a32d",
+    "url": "/monsters-rolodex/static/js/runtime-main.6559b9da.js"
+  }
+]);
